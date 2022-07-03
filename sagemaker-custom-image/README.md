497cbbb3776e1a435a42ba454d20ee8e72dd46dc
Max Reis <maxreis86@gmail.com>
2022-07-03 13:43:35 +0000

    Docker Readme

---
+++ sagemaker-custom-image/README.md
@@ -0,0 +1,4 @@
+# Build your own image using Docker and AWS ECR
+
+
+You can use AWS Cloud9 to execute the commands in "deploy_docker.sh" to create your custom image in ECR and attach it to SageMaker as shown [here](https://docs.aws.amazon.com/sagemaker/latest/dg/studio-byoi-attach.html)
--- sagemaker-custom-image/deploy_docker.sh
+++ sagemaker-custom-image/deploy_docker.sh
@@ -1,5 +1,7 @@
+#Put your AWS Account ID in "xxxxxxxxxxxx"
+
 sudo dockerd
-sudo aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 245799943033.dkr.ecr.us-east-1.amazonaws.com
+sudo aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin xxxxxxxxxxxx.dkr.ecr.us-east-1.amazonaws.com
 sudo docker build -t data-science .
-sudo docker tag data-science:latest 245799943033.dkr.ecr.us-east-1.amazonaws.com/data-science:latest
-sudo docker push 245799943033.dkr.ecr.us-east-1.amazonaws.com/data-science:latest
+sudo docker tag data-science:latest xxxxxxxxxxxx.dkr.ecr.us-east-1.amazonaws.com/data-science:latest
+sudo docker push xxxxxxxxxxxx.dkr.ecr.us-east-1.amazonaws.com/data-science:latest
